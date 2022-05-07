aa94292a363da34792aa67fee88b6bd6eb684700
Tiago Silva <thyago29.05@gmail.com>
2022-05-06 23:04:31 -0300

    Conclusão do tableHeader

--- src/components/TransationsList/TableHeader/index.js
+++ src/components/TransationsList/TableHeader/index.js
@@ -1,15 +1,55 @@
 import React from "react";
+import ArrowDown from "../../../assets/Arrow-down.svg";
+import ArrowUp from "../../../assets/Arrow-up.svg";
 import "./styles.css";
 
 const TableHeader = () => {
+  const [filter, setFilter] = React.useState("date");
+  const [order, setOrder] = React.useState("asc");
+
+  function handleChangeType(type) {
+    if (filter === type) {
+      setOrder(order === "asc" ? "desc" : "asc");
+    }
+    setFilter(type);
+  }
+
   return (
     <div className="table-header">
-      <div className="column-title">Data</div>
-      <div className="column-title">Dia da semana</div>
-      <div className="column-title">Descrição</div>
-      <div className="column-title">Categoria</div>
-      <div className="column-title">Valor</div>
-      <div className="column-title">Extra</div>
+      <div
+        className="column-title cursor-pointer"
+        onClick={() => handleChangeType("date")}
+      >
+        <span>Data</span>
+        {filter === "date" && (
+          <img src={order === "asc" ? ArrowUp : ArrowDown} alt="Arrow up"></img>
+        )}
+      </div>
+      <div
+        className="column-title cursor-pointer"
+        onClick={() => handleChangeType("weekday")}
+      >
+        <span>Dia da semana</span>
+        {filter === "weekday" && (
+          <img src={order === "asc" ? ArrowUp : ArrowDown} alt="Arrow up"></img>
+        )}
+      </div>
+      <div className="column-title">
+        <span>Descrição</span>
+      </div>
+      <div className="column-title">
+        <span>Categoria</span>
+      </div>
+      <div
+        className="column-title cursor-pointer"
+        onClick={() => handleChangeType("value")}
+      >
+        <span>Valor</span>
+        {filter === "value" && (
+          <img src={order === "asc" ? ArrowUp : ArrowDown} alt="Arrow up"></img>
+        )}
+      </div>
+      <div className="column-title"></div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/global.css";
 import Main from "./pages/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
